3ec6639c000b9ff5030faa0034cead270ecb9eea
Muhammad Ali <reddragon33795@gmail.com>
2022-10-14 16:22:43 +0500

    Minor Change Image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'tableImage': "url('./img/tableBlack.jpg')",
+        'tableImage': "url('./img/PinkBG-1080p.jpg')",
         'Bgimage': "url('./img/BGblue.png')",
         'tableImageTheme2': "url('./img/tableBlack.jpg')",
         'BgimageTheme2': "url('./img/blackGoldBG.jpg')",
